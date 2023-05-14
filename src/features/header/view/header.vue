2fd93bcdc3f3370e264c3b5c6222a174f14c015e
e.kubeckiy <e.kubeckiy@sports.ru>
2023-05-14 13:19:14 +0700

    fix: avatar placeholder src

--- src/features/header/view/header.vue
+++ src/features/header/view/header.vue
@@ -44,7 +44,7 @@ const handleSearchWord = ({target}: Event) => {
 			<div class="dropdown dropdown-end">
 				<label tabindex="0" class="btn btn-ghost btn-circle avatar">
 					<div class="w-10 rounded-full">
-						<img :src="userInfo ? userInfo.picture : '/img/icons/person.svg'" referrerpolicy="no-referrer" />
+						<img :src="userInfo ? userInfo.picture : '/focu/img/icons/person.svg'" referrerpolicy="no-referrer" />
 					</div>
 				</label>
 				<ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
