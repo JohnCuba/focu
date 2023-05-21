c6279a2f1bf2ee6a9b0872e754f7f2e785d434d4
JohnCuba <ekubetsky@gmail.com>
2023-05-21 14:21:41 +0700

    feat: add sw caching

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,15 @@ export default defineConfig({
   plugins: [
 		Vue(),
 		Pages(),
+		VitePWA({
+			injectRegister: 'auto',
+			registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+			devOptions: {
+        enabled: true
+      },
+		}),
 	],
 })
